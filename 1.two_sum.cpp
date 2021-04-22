c3891781803379ee4d47eea4240a9397c98c0289
Xinlong Yi <xinlong1996@gmail.com>
2021-04-22 14:28:23 -0700

    1 video

--- 1.two_sum.cpp
+++ 1.two_sum.cpp
@@ -3,24 +3,24 @@
 
 using namespace std;
 
-/**
- * Use a hashmap to store the map from value to index. Go through every element,
- * use hashmap to check if "diff" met or not. If met, save the answer.
- */
 class Solution {
 public:
   vector<int> twoSum(vector<int> &nums, int target) {
-    unordered_map<int, int> memo; // Map value to index
     vector<int> ans;
+    unordered_map<int, int> memo;
     for (int i = 0; i < nums.size(); i++) {
       int diff = target - nums[i];
+
+      // Find
       if (memo.find(diff) != memo.end()) {
         ans.emplace_back(memo[diff]);
         ans.emplace_back(i);
-        break;
+        return ans;
       }
+
       memo[nums[i]] = i;
     }
+
     return ans;
   }
 };
