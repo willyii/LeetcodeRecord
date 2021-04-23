a614a017b1da93f9e8d682c2162945494bbd5c85
Xinlong Yi <xinlong1996@gmail.com>
2021-04-23 10:40:23 -0700

    2 video

--- 2.add_two_numbers.cpp
+++ 2.add_two_numbers.cpp
@@ -1,46 +1,44 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// struct ListNode {
+// int val;
+// ListNode *next;
+// ListNode() : val(0), next(nullptr) {}
+// ListNode(int x) : val(x), next(nullptr) {}
+// ListNode(int x, ListNode *next) : val(x), next(next) {}
+//};
 
-/**
- * Using an bool "carry" to indicate should add one or not. Then go over every
- * digit.
- */
 class Solution {
 public:
   ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
     ListNode *ans = new ListNode(), *ans_head = ans;
     bool carry = false;
+
     while (l1 != nullptr || l2 != nullptr) {
-      if (l1 != nullptr && l2 != nullptr) {
-        int add = l1->val + l2->val + carry;
-        carry = (add >= 10);
-        ans->next = new ListNode(add % 10);
-        l1 = l1->next;
-        l2 = l2->next;
-      } else if (l1 == nullptr) {
+      if (l1 == nullptr) {
         int add = l2->val + carry;
-        carry = (add >= 10);
         ans->next = new ListNode(add % 10);
+        carry = (add >= 10);
+        ans = ans->next;
         l2 = l2->next;
-      } else {
+      } else if (l2 == nullptr) {
         int add = l1->val + carry;
-        carry = (add >= 10);
         ans->next = new ListNode(add % 10);
+        carry = (add >= 10);
+        ans = ans->next;
         l1 = l1->next;
+      } else {
+        int add = l1->val + carry + l2->val;
+        ans->next = new ListNode(add % 10);
+        carry = (add >= 10);
+        ans = ans->next;
+        l1 = l1->next;
+        l2 = l2->next;
       }
-      ans = ans->next;
     }
+
     if (carry) {
       ans->next = new ListNode(1);
     }
+
     return ans_head->next;
   }
 };
