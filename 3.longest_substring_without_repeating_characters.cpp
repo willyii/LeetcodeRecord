ef2c00e6d7bd88fe8d1c594ad4ef290d7a2cb42a
Xinlong Yi <xinlong1996@gmail.com>
2021-04-26 10:08:56 -0700

    3 video

--- 3.longest_substring_without_repeating_characters.cpp
+++ 3.longest_substring_without_repeating_characters.cpp
@@ -1,35 +1,28 @@
-#include <algorithm>
 #include <string>
 #include <unordered_map>
 
 using namespace std;
 
-/**
- * I plan to use an array with 26 ints to store number of occurance of each
- * char. Then I use two pointer to indicate the left end and right end of
- * subarray.
- */
 class Solution {
 public:
   int lengthOfLongestSubstring(string s) {
-    if (s.size() == 0 || s.size() == 1)
-      return s.size();
+    if (s.length() == 0 || s.length() == 1)
+      return s.length();
 
     unordered_map<char, int> memo;
-    for (char c : s)
+    for (auto c : s)
       memo[c] = 0;
-    int left_end = 0, right_end = 0, ans = 1;
 
-    while (right_end < s.size()) {
-      memo[s[right_end]] += 1;
+    int l = 0, r = 0, ans = 1;
+    while (r < s.length()) {
+      memo[s[r]] += 1;
 
-      while (memo[s[right_end]] > 1) { // visited
-        memo[s[left_end]] -= 1;
-        left_end += 1;
+      while (memo[s[r]] > 1) {
+        memo[s[l++]] -= 1;
       }
 
-      right_end++;
-      ans = std::max(ans, right_end - left_end);
+      r++;
+      ans = max(ans, r - l);
     }
 
     return ans;
