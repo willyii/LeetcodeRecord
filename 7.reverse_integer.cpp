875d059b3ee2b86ee0c1779acc91d8f7758efbc0
Xinlong Yi <xinlong1996@gmail.com>
2021-05-02 14:13:46 -0700

    7 video

--- 7.reverse_integer.cpp
+++ 7.reverse_integer.cpp
@@ -1,19 +1,26 @@
 #include <climits>
+
 using namespace std;
 
 class Solution {
 public:
   int reverse(int x) {
-    int rev = 0;
+    int ans = 0;
     while (x != 0) {
       int pop = x % 10;
-      x /= 10;
-      if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && pop > 7))
+      x = x / 10;
+
+      // upper overflow
+      if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && pop > 7)) {
         return 0;
-      if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && pop < -8))
+      }
+      // lower overflow
+      if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && pop > 8)) {
         return 0;
-      rev = rev * 10 + pop;
+      }
+
+      ans = ans * 10 + pop;
     }
-    return rev;
+    return ans;
   }
 };
