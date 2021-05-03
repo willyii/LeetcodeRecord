03f02f46cd32ad3607f6028e3305e25f96ad5d52
Xinlong Yi <xinlong1996@gmail.com>
2021-05-03 11:05:13 -0700

    8 video

--- 8.string_to_integer_atoi.cpp
+++ 8.string_to_integer_atoi.cpp
@@ -6,15 +6,14 @@ using namespace std;
 class Solution {
 public:
   int myAtoi(string s) {
-    int ans = 0;
-    int sign = 1;
-    int idx = 0;
+    int ans = 0, idx = 0;
 
+    // Leading whitespace
     while (idx < s.length() && s[idx] == ' ')
-      // leading whitespace
       idx++;
 
-    // leading sign
+    // sign
+    int sign = 1;
     if (s[idx] == '-') {
       sign = -1;
       idx++;
@@ -23,13 +22,13 @@ public:
     }
 
     while (idx < s.length() && s[idx] >= '0' && s[idx] <= '9') {
-
-      // overflow
+      // if overflow
       if (ans > INT_MAX / 10 ||
-          (ans == INT_MAX / 10 && s[idx] - '0' > INT_MAX % 10)) {
-        return (sign == 1) ? INT_MAX : INT_MIN;
-      }
-      ans = ans * 10 + (s[idx++] - '0');
+          (ans == INT_MAX / 10 && s[idx] - '0' > INT_MAX % 10))
+        return sign == 1 ? INT_MAX : INT_MIN;
+
+      ans = ans * 10 + (s[idx] - '0');
+      idx++;
     }
 
     return ans * sign;
